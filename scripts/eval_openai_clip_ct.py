5ad2cecf1c27192630fe3bacbad489de1ca91616
Katharina <katharina.prasse@posteo.de>
2024-06-04 19:48:19 +0200

    save conf mat, shorten code

--- scripts/eval_openai_clip_ct.py
+++ scripts/eval_openai_clip_ct.py
@@ -8,9 +8,10 @@ import os
 from torch.utils.data import DataLoader, Dataset
 import glob
 from collections import Counter
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, balanced_accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, confusion_matrix, ConfusionMatrixDisplay, precision_recall_fscore_support
 import numpy as np
 import clip
+import matplotlib.pyplot as plt
 
 # load model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -132,7 +133,7 @@ def evaluate_category(category, text, class_overview, img_dir, preprocess, top_k
     class_wise_accuracy = {class_name: correct_per_class[class_name] / total_per_class[class_name] 
                            for class_name in total_per_class}
     print(class_wise_accuracy)
-    class_metrics = compute_classwise_metrics(true_labels, predicted_labels, idx_to_label)
+    class_metrics = compute_classwise_metrics(true_labels, predicted_labels, idx_to_label, category)
 
     
     return class_metrics, overall_metrics
@@ -142,9 +143,7 @@ def compute_overall_metrics(true_labels, pred_labels, cat):
     print(f"Accuracy for {cat}: {accuracy:.4f}")
 
     balanced_accuracy = balanced_accuracy_score(true_labels, pred_labels)
-    precision_macro = precision_score(true_labels, pred_labels, average='macro')
-    recall_macro = recall_score(true_labels, pred_labels, average='macro')
-    f1_macro = f1_score(true_labels, pred_labels, average='macro')
+    precision_macro, recall_macro, f1_macro, _ = precision_recall_fscore_support(true_labels, pred_labels, average="macro")
 
     overall_metrics = {
         'Balanced Accuracy': balanced_accuracy,
@@ -156,30 +155,40 @@ def compute_overall_metrics(true_labels, pred_labels, cat):
 
     return overall_metrics
 
-def compute_classwise_metrics(true_labels, pred_labels, mapping):
-    precision = precision_score(true_labels, pred_labels, average=None)
-    recall = recall_score(true_labels, pred_labels, average=None)
-    f1 = f1_score(true_labels, pred_labels, average=None)
+def compute_classwise_metrics(true_labels, pred_labels, mapping, topic):
+    prec, recall, f1, support = precision_recall_fscore_support(true_labels, pred_labels, average=None)
+    print(mapping)
 
     # compuate and save confusion matrix
     conf_matrix = confusion_matrix(true_labels, pred_labels, labels=list(mapping.keys()), normalize="true")
     class_wise_acc = np.diag(conf_matrix)
+    save_convmat(conf_matrix, topic, list(mapping.values()))
 
     #top_k_accuracy = sum(1 for true, pred in zip(true_labels, predicted_labels_top_k) if true in pred) / len(true_labels)
         
     class_metrics = {
-        mapping[i]: {'Accuracy':class_wise_acc[i],'Precision': precision[i], 'Recall': recall[i], 'F1 Score': f1[i]}
-        for i in range(len(precision))}
+        mapping[i]: {'Accuracy':class_wise_acc[i],'Precision': prec[i], 'Recall': recall[i], 'F1 Score': f1[i], 'Support':support[i]}
+        for i in range(len(prec))}
     
     return class_metrics
 
+def save_convmat(conv_mat, topic, label_overview):
+        plt.rcParams.update({'font.size': 20})
+        disp = ConfusionMatrixDisplay(confusion_matrix=conv_mat, display_labels=label_overview)
+        _, ax = plt.subplots(figsize=(10,10))
+        
+        disp.plot(ax=ax, xticks_rotation="vertical", colorbar=False, cmap="Greens",values_format=".1f")
+
+        for i in disp.text_.flatten(): i._text = i._text.replace("0.", ".")
+
+        #plt.title("Normalized confusion matrix")
+        plt.savefig(f"conf_mat/openai_clip_vitb32/conv_mat_{topic}_norm.png", bbox_inches="tight")
+        plt.close()
 
 # Main execution
 cat_dir = glob.glob("../../ClimateVisions_Dataset/categories/*.txt")
 text, class_overview = process_text_descriptions(cat_dir)
 img_dir = "../../Images/ClimateCT"
-
-overall_top_k_accuracy = []
 
 for category in text.keys():
     class_metrics, overall_metrics  = evaluate_category(category, text, class_overview, img_dir, preprocess, top_k=3)
@@ -187,12 +196,15 @@ for category in text.keys():
     
     print(f"Class-wise Metrics for {category}:")
     for class_name, metrics in class_metrics.items():
-        print(f"  {class_name}: Accuracy={metrics['Accuracy']:.4f}, Precision={metrics['Precision']:.4f}, Recall={metrics['Recall']:.4f}, F1 Score={metrics['F1 Score']:.4f}")
+        print(f"  {class_name}: Accuracy={metrics['Accuracy']:.4f}, Precision={metrics['Precision']:.4f}, Recall={metrics['Recall']:.4f}, F1 Score={metrics['F1 Score']:.4f}, Support={metrics['Support']}")
+    print()
+    print(f"  Overall balanced accuracy (Macro): {overall_metrics['Balanced Accuracy']:.4f}")
     print(f"  Overall Precision (Macro): {overall_metrics['Precision (Macro)']:.4f}")
     print(f"  Overall Recall (Macro): {overall_metrics['Recall (Macro)']:.4f}")
     print(f"  Overall F1 Score (Macro): {overall_metrics['F1 Score (Macro)']:.4f}")
     #print(f"  Top-3 Accuracy: {overall_metrics['Top-k Accuracy']:.4f}")
     print()
+    break
 
 #average_top_k_accuracy = sum(overall_top_k_accuracy) / len(overall_top_k_accuracy)
 #print("Average Top-k Accuracy across all categories:", average_top_k_accuracy)
