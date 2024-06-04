ab89b38e6b85e7804d3a66af2a7b32c968778041
Katharina <katharina.prasse@posteo.de>
2024-06-04 20:17:06 +0200

    bug fix

--- scripts/eval_clipb32_quickgelu_ct.py
+++ scripts/eval_clipb32_quickgelu_ct.py
@@ -3,15 +3,14 @@
 # load packages
 import torch
 from PIL import Image
-import open_clip
 import os
 #from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, Dataset
 import glob
-from collections import Counter
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, balanced_accuracy_score, confusion_matrix
-from torch.utils.tensorboard import SummaryWriter
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, confusion_matrix, ConfusionMatrixDisplay, precision_recall_fscore_support
 import numpy as np
+import open_clip
+import matplotlib.pyplot as plt
 
 # load model
 model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32-quickgelu', pretrained='laion400m_e32') # quickgelu is used by openai
@@ -71,37 +70,29 @@ def process_text_descriptions(cat_dir):
             class_desc = [line.strip().split(":") for line in f]
         queries = [desc[1][1:] for desc in class_desc] # remove white space before query
         text[category] = tokenizer(queries)
-        # create a mapping that maps the closest query to the corresponding class (some classes have multiple queries)
-        classes = [desc[0] for desc in class_desc]
-        class_list = list(dict.fromkeys(classes))
-        class_dict = {cls: idx for idx, cls in enumerate(class_list)}
-        query_class_mapping = [class_dict[cls] for cls in classes]
-        class_overview[category] = dict(zip(range(len(queries)), query_class_mapping))
+        # create a mapping from query to class
+        class_overview[category] ={desc[1][1:]: desc[0] for desc in class_desc}
     return text, class_overview
 
 # Evaluate a super-class
-def evaluate_category(category, text, class_overview, img_dir, preprocess, top_k, writer = None):
+def evaluate_category(category, text, class_overview, img_dir, preprocess, top_k):
     # load annotations
     txt_file = f"../../ClimateVisions_Dataset/final_labels/tum_{category}.txt"
     dataset = CustomImageDataset(txt_file=txt_file, img_dir=img_dir, transform=preprocess)
     dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
     
     label_to_idx, idx_to_label = dataset.get_label_mappings()
-    query_class_mapping = class_overview[category]
-    class_list = list(dict.fromkeys(idx_to_label))
+    query_class_overview = class_overview[category]
 
-    # required for class_wise accuracy
-    total_per_class = Counter()
-    correct_per_class = Counter()
+    # create mapping two combine results for classes with multiple queries
+    query_class_map = dict(zip(np.arange(0,len(query_class_overview),1),query_class_overview.values()))
+    query_class_mapping = {k:label_to_idx[v] for (k,v) in query_class_map.items()}
 
     # required for recall, precision and F1
     true_labels = []
     predicted_labels = []
     predicted_labels_top_k = []
     
-    # required for total accuracy
-    correct, total = 0, 0
-
     with torch.no_grad():
         for images, labels in dataloader:
             # load embeddings
@@ -119,42 +110,23 @@ def evaluate_category(category, text, class_overview, img_dir, preprocess, top_k
             query_preds = torch.argmax(text_probs, dim=1).tolist()
             class_preds = torch.tensor([query_class_mapping[q] for q in query_preds]).to(device)
             
-            # overall accuracy
-            total += labels.size(0)
-            correct += (class_preds == labels).sum().item()
-
             # other metrics
             true_labels.extend(labels.cpu().numpy())
             predicted_labels.extend(class_preds.cpu().numpy())
             predicted_labels_top_k.extend(class_preds_top_k.cpu().numpy())
-
-            # class-wise accuracy
-            for label, pred in zip(labels, class_preds):
-                total_per_class[idx_to_label[label.item()]] += 1
-                if label == pred:
-                    correct_per_class[idx_to_label[label.item()]] += 1
     
-    # overall metrics
-    accuracy = correct / total
-    print(f"Accuracy for {category}: {accuracy:.4f}")
-    accuracy = accuracy_score(true_labels, predicted_labels)
-    print(accuracy)
-    overall_metrics = compute_overall_metrics(true_labels, predicted_labels, class_list)
-
-    # class-wise metrics
-    class_wise_accuracy = {label_to_idx[class_name]: correct_per_class[class_name] / total_per_class[class_name] 
-                           for class_name in total_per_class}
-    print(class_wise_accuracy)
-    class_metrics = compute_classwise_metrics(true_labels, predicted_labels, class_wise_accuracy, idx_to_label)
-
     
+    overall_metrics = compute_overall_metrics(true_labels, predicted_labels, category)
+    class_metrics = compute_classwise_metrics(true_labels, predicted_labels, idx_to_label, category)
+
     return class_metrics, overall_metrics
 
-def compute_overall_metrics(true_labels, pred_labels, class_labels):
+def compute_overall_metrics(true_labels, pred_labels, cat):
+    accuracy = accuracy_score(true_labels, pred_labels)
+    print(f"Accuracy for {cat}: {accuracy:.4f}")
+
     balanced_accuracy = balanced_accuracy_score(true_labels, pred_labels)
-    precision_macro = precision_score(true_labels, pred_labels, average='macro')
-    recall_macro = recall_score(true_labels, pred_labels, average='macro')
-    f1_macro = f1_score(true_labels, pred_labels, average='macro')
+    precision_macro, recall_macro, f1_macro, _ = precision_recall_fscore_support(true_labels, pred_labels, average="macro")
 
     overall_metrics = {
         'Balanced Accuracy': balanced_accuracy,
@@ -163,32 +135,42 @@ def compute_overall_metrics(true_labels, pred_labels, class_labels):
         'F1 Score (Macro)': f1_macro#,
         #'Top-k Accuracy': top_k_accuracy
         }
-    # compuate and save confusion matrix
-    confusion_matrix(true_labels, pred_labels, labels= class_labels)
-    class_wise_acc = np.diag(confusion_matrix)
+
     return overall_metrics
 
-def compute_classwise_metrics(true_labels, pred_labels, class_wise_accuracy, mapping):
-    precision = precision_score(true_labels, pred_labels, average=None)
-    recall = recall_score(true_labels, pred_labels, average=None)
-    f1 = f1_score(true_labels, pred_labels, average=None)
-    acc = accuracy_score(true_labels, pred_labels)
-    print(acc)
+def compute_classwise_metrics(true_labels, pred_labels, mapping, topic):
+    prec, recall, f1, support = precision_recall_fscore_support(true_labels, pred_labels, average=None)
+
+    # compuate and save confusion matrix
+    conf_matrix = confusion_matrix(true_labels, pred_labels, labels=list(mapping.keys()), normalize="true")
+    class_wise_acc = np.diag(conf_matrix)
+    save_convmat(conf_matrix, topic, list(mapping.values()))
+
     #top_k_accuracy = sum(1 for true, pred in zip(true_labels, predicted_labels_top_k) if true in pred) / len(true_labels)
         
     class_metrics = {
-        mapping[i]: {'Accuracy':acc[i],'Precision': precision[i], 'Recall': recall[i], 'F1 Score': f1[i]}
-        for i in range(len(precision))}
+        mapping[i]: {'Accuracy':class_wise_acc[i],'Precision': prec[i], 'Recall': recall[i], 'F1 Score': f1[i], 'Support':support[i]}
+        for i in range(len(prec))}
     
     return class_metrics
 
+def save_convmat(conv_mat, topic, label_overview):
+        plt.rcParams.update({'font.size': 20})
+        disp = ConfusionMatrixDisplay(confusion_matrix=conv_mat, display_labels=label_overview)
+        _, ax = plt.subplots(figsize=(10,10))
+        
+        disp.plot(ax=ax, xticks_rotation="vertical", colorbar=False, cmap="Greens",values_format=".1f")
+
+        for i in disp.text_.flatten(): i._text = i._text.replace("0.", ".")
+
+        #plt.title("Normalized confusion matrix")
+        plt.savefig(f"conf_mat/openclip_clip_vitb32_quickgelu/conv_mat_{topic}_norm.png", bbox_inches="tight")
+        plt.close()
 
 # Main execution
 cat_dir = glob.glob("../../ClimateVisions_Dataset/categories/*.txt")
 text, class_overview = process_text_descriptions(cat_dir)
 img_dir = "../../Images/ClimateCT"
-
-overall_top_k_accuracy = []
 
 for category in text.keys():
     class_metrics, overall_metrics  = evaluate_category(category, text, class_overview, img_dir, preprocess, top_k=3)
@@ -196,7 +178,9 @@ for category in text.keys():
     
     print(f"Class-wise Metrics for {category}:")
     for class_name, metrics in class_metrics.items():
-        print(f"  {class_name}: Accuracy={metrics['Accuracy']:.4f}, Precision={metrics['Precision']:.4f}, Recall={metrics['Recall']:.4f}, F1 Score={metrics['F1 Score']:.4f}")
+        print(f"  {class_name}: Accuracy={metrics['Accuracy']:.4f}, Precision={metrics['Precision']:.4f}, Recall={metrics['Recall']:.4f}, F1 Score={metrics['F1 Score']:.4f}, Support={metrics['Support']}")
+    print()
+    print(f"  Overall balanced accuracy (Macro): {overall_metrics['Balanced Accuracy']:.4f}")
     print(f"  Overall Precision (Macro): {overall_metrics['Precision (Macro)']:.4f}")
     print(f"  Overall Recall (Macro): {overall_metrics['Recall (Macro)']:.4f}")
     print(f"  Overall F1 Score (Macro): {overall_metrics['F1 Score (Macro)']:.4f}")
@@ -205,6 +189,3 @@ for category in text.keys():
 
 #average_top_k_accuracy = sum(overall_top_k_accuracy) / len(overall_top_k_accuracy)
 #print("Average Top-k Accuracy across all categories:", average_top_k_accuracy)
-
-# Close SummaryWriter
-writer.close()
